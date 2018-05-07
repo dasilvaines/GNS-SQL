7cd5e8215c84ba1bd57562d8fef38c7cd451648b
WILMANNflorian <Florian.Wilmann.Sio@gmail.com>
2018-05-07 16:14:52 +0200

    Script création BD

--- gns.sql
+++ gns.sql
@@ -48,3 +48,44 @@ ALTER TABLE Partie ADD CONSTRAINT FK_Partie_initier  FOREIGN KEY (initier) REFER
 ALTER TABLE Partie ADD CONSTRAINT FK_Partie_adversaire  FOREIGN KEY (adversaire) REFERENCES Joueur(numeroJoueur);
 ALTER TABLE Partie ADD CONSTRAINT FK_Partie_vainqueur FOREIGN KEY (vainqueur) REFERENCES Joueur(numeroJoueur);
 ALTER TABLE Partie ADD CONSTRAINT FK_Partie_suivant FOREIGN KEY (suivant) REFERENCES Joueur(numeroJoueur);
+
+
+
+
+
+insert into Joueur values ( 1 , "Nicolas" , "azerty");
+insert into Joueur values ( 2 , "Ilona" , "azerty");
+insert into Joueur values ( 3 , "Georges" , "azerty");
+insert into Joueur values ( 4 , "Aicha" , "azerty");
+insert into Joueur values ( 5 , "Cody" , "azerty");
+
+
+insert into Couleur values (1 ,"Blanc");
+insert into Couleur values (2, "Noir");
+
+
+insert into Partie values ( 1 , "01/05/18" , "Cody" , "Ilona" , "Ilona" , , "Blanc" , "Noir" );
+insert into Partie values ( 2 , "01/05/18" , "Cody" , "Ilona" , "Cody" , , "Noir" , "Blanc" );
+insert into Partie values ( 3 , "01/05/18" , "Cody" ,  , , "Cody" , "Blanc" ,  );
+insert into Partie values ( 4 , "01/05/18" , "Ilona" ,  ,  , , "Noir" , );
+insert into Partie values ( 5 , "02/05/18" , "Cody" , "Nicolas" ,  ,"Nicolas" , "Blanc" , "Noir" );
+insert into Partie values ( 6 , "02/05/18" , "Cody" , "Nicolas" , ,"Cody"  , "Blanc" , "Noir" );
+insert into Partie values ( 7 , "02/05/18" , "Nicolas" , ,  , ,  , "Noir" );
+insert into Partie values ( 8 , "02/05/18" , "Nicolas" ,  ,  , ,  , "Noir" );
+insert into Partie values ( 9 , "03/05/18" , "Cody" , "Ilona" ,  , "Ilona" , "Blanc" , "Noir" );
+insert into Partie values ( 10 , "03/05/18" , "Ilona" , "Nicolas" , "Ilona" , , "Noir" , "Blanc" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
